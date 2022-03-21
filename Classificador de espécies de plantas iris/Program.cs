91a5c92020edb4e1da0b0544eea7e26a1afe41c7
karizune <luanchrystian2@gmail.com>
2022-03-21 15:40:19 -0300

    primeiro commit contendo o básico para o funcionamento da machine learning para classificar os 3 tipos de flores iris

---
+++ Classificador de espécies de plantas iris/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Classificador_de_espécies_de_plantas_irisML.Model;
+
+
+namespace Classificador_de_espécies_de_plantas_iris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
